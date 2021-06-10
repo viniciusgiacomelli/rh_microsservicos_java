3830a72beccf2fe4f2724dea064757092d722287
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-10 18:14:16 -0300

    Microsservico rh-funcionario iniciado

---
+++ rh-funcionario/src/main/java/com/microsservico/funcionario/RhFuncionarioApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.funcionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhFuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhFuncionarioApplication.class, args);
+	}
+
+}
---
+++ rh-funcionario/src/test/java/com/microsservico/funcionario/RhFuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservico.funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhFuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

10ab9d5e0b12fdede25ab5bd3dc14db8227cec5a
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-11 17:35:01 -0300

    Eureka Server criado

---
+++ rh-eureka-server/src/main/java/com/microsservico/eurekaserver/RhEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhEurekaServerApplication.class, args);
+	}
+
+}
---
+++ rh-eureka-server/src/test/java/com/microsservico/eurekaserver/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservico.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

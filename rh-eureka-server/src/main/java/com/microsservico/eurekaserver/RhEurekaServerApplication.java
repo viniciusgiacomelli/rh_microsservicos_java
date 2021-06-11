44e1375fe979680486a0d405e6fdab8578429b35
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-11 17:46:57 -0300

    Eureka Server Configurado

--- rh-eureka-server/src/main/java/com/microsservico/eurekaserver/RhEurekaServerApplication.java
+++ rh-eureka-server/src/main/java/com/microsservico/eurekaserver/RhEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.microsservico.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class RhEurekaServerApplication {
 

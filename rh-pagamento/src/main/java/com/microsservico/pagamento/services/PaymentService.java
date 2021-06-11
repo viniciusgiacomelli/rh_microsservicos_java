18b4ac4ba47908479d1c692eed5bc6a79d33bfcd
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-11 11:49:22 -0300

    RestTemplate

--- rh-pagamento/src/main/java/com/microsservico/pagamento/services/PaymentService.java
+++ rh-pagamento/src/main/java/com/microsservico/pagamento/services/PaymentService.java
@@ -1,15 +1,31 @@
 package com.microsservico.pagamento.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.microsservico.pagamento.entities.Payment;
+import com.microsservico.pagamento.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${rh-funcionario}")
+	private String hostFuncionario;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		/**Buscar worker do BD**/
-		return new Payment("Bob", 200.0, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(hostFuncionario+"/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }

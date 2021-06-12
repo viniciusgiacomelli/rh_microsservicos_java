8ab7b1c06cf85cda29322591e13bf60f61aafa02
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-12 12:28:35 -0300

    EurekaClients configurados

--- rh-funcionario/src/main/java/com/microsservico/funcionario/RhFuncionarioApplication.java
+++ rh-funcionario/src/main/java/com/microsservico/funcionario/RhFuncionarioApplication.java
@@ -2,7 +2,9 @@ package com.microsservico.funcionario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RhFuncionarioApplication {
 

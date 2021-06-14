acdb7e9405c8f8b57f77424a9f3f4e295cadb3fa
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-14 12:37:51 -0300

    Zuul API Gateway criado

---
+++ rh-api-gateway-zuul/src/main/java/com/microsservico/rhapigatewayzuul/RhApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.rhapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ rh-api-gateway-zuul/src/test/java/com/microsservico/rhapigatewayzuul/RhApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservico.rhapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

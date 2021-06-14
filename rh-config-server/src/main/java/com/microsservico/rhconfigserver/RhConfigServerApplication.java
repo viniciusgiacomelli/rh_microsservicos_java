70a6b93f9bf90da8d75daf920cc797112784f2f6
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-14 17:27:06 -0300

    Servidor Configuracao criado

---
+++ rh-config-server/src/main/java/com/microsservico/rhconfigserver/RhConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.rhconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhConfigServerApplication.class, args);
+	}
+
+}
---
+++ rh-config-server/src/test/java/com/microsservico/rhconfigserver/RhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microsservico.rhconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

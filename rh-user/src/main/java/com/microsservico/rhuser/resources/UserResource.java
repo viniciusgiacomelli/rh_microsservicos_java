bafdf7a4878ac12b21e6ba1859d5f4687bf20717
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-15 16:49:56 -0300

    UserRepository, UserResource, Zuul configurado

---
+++ rh-user/src/main/java/com/microsservico/rhuser/resources/UserResource.java
@@ -0,0 +1,33 @@
+package com.microsservico.rhuser.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microsservico.rhuser.entities.User;
+import com.microsservico.rhuser.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = repository.findById(id).get();
+		return ResponseEntity.ok(obj);
+	}
+	
+	@GetMapping(value = "/search")
+	public ResponseEntity<User> findById(@RequestParam String email) {
+		User obj = repository.findByEmail(email);
+		return ResponseEntity.ok(obj);
+	}
+	
+}

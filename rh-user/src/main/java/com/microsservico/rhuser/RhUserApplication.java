e0e9acb3a76c47f6d04eca888cfbaa1597b3b67f
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-15 14:23:37 -0300

    Microsservico rh-usuario iniciado

---
+++ rh-user/src/main/java/com/microsservico/rhuser/RhUserApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.rhuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhUserApplication.class, args);
+	}
+
+}

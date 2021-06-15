cb34075b1c2ae532ca834d2364b3c9d7177cd598
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-15 14:54:55 -0300

    Entidades: user, role e associacao muitos para muitos

---
+++ rh-user/src/main/java/com/microsservico/rhuser/entities/Role.java
@@ -0,0 +1,74 @@
+package com.microsservico.rhuser.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+
+	static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	
+	public Role() {}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+}

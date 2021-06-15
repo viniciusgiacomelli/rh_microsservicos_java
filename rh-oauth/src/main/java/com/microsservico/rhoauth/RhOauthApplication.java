ec25a4d2651f67d15b5975e5db4516c823a5f8c2
Vinicius Giacomelli <giacomelli.vinicius@gmail.com>
2021-06-15 16:55:27 -0300

    Microsservico oauth iniciado

---
+++ rh-oauth/src/main/java/com/microsservico/rhoauth/RhOauthApplication.java
@@ -0,0 +1,13 @@
+package com.microsservico.rhoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhOauthApplication.class, args);
+	}
+
+}
